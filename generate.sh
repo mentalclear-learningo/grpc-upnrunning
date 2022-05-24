975595fc3b79068fc19ab2517a4edb21d5f1e968
Dmitrii Kilishek <mentalclear@gmail.com>
2022-05-24 00:23:17 -0400

    Continue with: Chapter 3. gRPC Communication Patterns TODO see generate.sh

--- generate.sh
+++ generate.sh
@@ -3,11 +3,16 @@
 protodir="ecommerce"
 protofile="${protodir}/product_info.proto"
 
+# TODO: Learn: 
+# 1. --go_out is it a clinet code? Can it be put into client folder only?
+# 2. --go-grpc_out is it a server code? Can it be put into server folder only?
 
 # Generate Go code 
 protoc --go_out=. --go_opt=paths=source_relative \
 --go-grpc_out=. --go-grpc_opt=paths=source_relative \
 $protofile
+
+
 
 # Generate Python code 
 # python3 -m grpc_tools.protoc -I$protodir \
